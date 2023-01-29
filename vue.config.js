c5eea2d49d033a1608a3a3b9f00d47f4a7a724c8
Adam Freiesleben <adam.freiesleben@juicymo.cz>
2023-01-29 22:00:09 +0100

    regex change

--- vue.config.js
+++ vue.config.js
@@ -6,17 +6,19 @@ module.exports = defineConfig({
       navigateFallback: "/index.html",
       skipWaiting: true,
       clientsClaim: true,
-      runtimeCaching: [{
-        urlPattern: new RegExp('APU_URL'),
-        handler: "NetworkFirst",
-        options: {
-          networkTimeoutSeconds: 20,
-          cacheName: "api-cache",
-          cacheableResponse: {
-            statuses: [0, 200],
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp("^https?://(www.)?thecocktaildb.com.*"),
+          handler: "NetworkFirst",
+          options: {
+            networkTimeoutSeconds: 20,
+            cacheName: "api-cache",
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
           }
         }
-      }]
+      ]
     },
     manifestOptions: {
       name: "Drinker",
@@ -24,7 +26,7 @@ module.exports = defineConfig({
       msTileColor: "#2c2c54",
       appleMobileWebAppCapable: "yes",
       appleMobileWebAppStatusBarStyle: "#2c2c54",
-      background_color: "#fff",
+      background_color: "#fff"
     }
   }
 });
